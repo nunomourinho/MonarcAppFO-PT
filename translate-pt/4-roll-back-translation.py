d02fe91d4483e3457c6d72dbb513b458a44503db
Nuno Mourinho <nuno.mourinho@gmail.com>
2024-08-28 14:43:48 +0000

    Added translation interface and Portuguese from Portugal translated strings

---
+++ translate-pt/4-roll-back-translation.py
@@ -0,0 +1,54 @@
+import csv
+from configparser import ConfigParser
+import mysql.connector
+from mysql.connector import Error
+from tqdm import tqdm
+
+def read_translations(file_path):
+    translations = []
+    with open(file_path, mode='r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='¨')
+        for row in reader:
+            translations.append(row)
+    return translations
+
+def read_db_config(config_file):
+    parser = ConfigParser()
+    parser.read(config_file)
+    db_config = {
+        'host': parser.get('mysql', 'host'),
+        'database': parser.get('mysql', 'database'),
+        'user': parser.get('mysql', 'user'),
+        'password': parser.get('mysql', 'password')
+    }
+    return db_config
+
+
+def update_database(translations, config):
+    try:
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor()
+        
+        for translation in tqdm(translations, desc="Atualizando banco de dados"):
+            try:
+                sql = f"UPDATE {translation['table']} SET {translation['field']} = %s WHERE {translation['field']} = %s"
+                values = (translation['original_string'], translation['translated_string'])
+                cursor.execute(sql, values)
+            except Error as e:
+                print(f"Erro ao executar a query para {translation['table']} com os valores {values}: {e}")
+                
+        connection.commit()
+    except Error as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Conexão MySQL fechada.")
+
+
+
+if __name__ == "__main__":
+    translations = read_translations('final_translated_strings.csv')
+    db_config = read_db_config('config.ini')
+    update_database(translations, db_config)
