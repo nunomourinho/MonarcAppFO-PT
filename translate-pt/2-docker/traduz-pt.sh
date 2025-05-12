343f75c461db9f5508a9597e145c2b266456db3c
Nuno Mourinho <56609351+nunomourinho@users.noreply.github.com>
2025-05-12 23:06:40 +0100

    Update traduz-pt.sh

--- translate-pt/2-docker/traduz-pt.sh
+++ translate-pt/2-docker/traduz-pt.sh
@@ -1,6 +1,6 @@
 docker exec -it monarc_apache apt install python3-pymysql -y
 docker exec -it monarc_apache apt install python3-tqdm -y
 docker exec -it monarc_apache python3 /usr/src/app/3-translate-db.py
-docker cp ./helper/pt/. monarc-apache:/var/lib/monarc/fo/deliveries/cases/EN/
-docker exec -it monarc-apache chown -R www-data:www-data /var/lib/monarc/fo/deliveries/cases/EN
-docker exec -it monarc-apache  chmod -R 755 /var/lib/monarc/fo/deliveries/cases/EN
+docker cp ./helper/pt/. monarc_apache:/var/lib/monarc/fo/deliveries/cases/EN/
+docker exec -it monarc_apache chown -R www-data:www-data /var/lib/monarc/fo/deliveries/cases/EN
+docker exec -it monarc_apache  chmod -R 755 /var/lib/monarc/fo/deliveries/cases/EN
